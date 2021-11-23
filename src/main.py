502d3facd6b8f07aac3e08c6062efdf03e7b2684
Greg Wolf <greg.d.wolf@gmail.com>
2021-11-22 23:15:07 -0600

    Cleanup and reorganization

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Fri Nov 19 11:26:06 2021
 
@@ -13,8 +11,8 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
-#used for GUI
-import tkinter as tk 
+# used for GUI
+import tkinter as tk
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
@@ -37,45 +35,47 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                '../res/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open('../res/token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('calendar', 'v3', credentials=creds)
 
     # Call the Calendar API
-    def getEvents():
-        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    def get_events():
+        # 'Z' indicates UTC time
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
         print('Getting the upcoming 10 events')
         lbl_showEvents["text"] = 'Upcoming 10 events:'
         events_result = service.events().list(calendarId='primary', timeMin=now,
-                                            maxResults=10, singleEvents=True,
-                                            orderBy='startTime').execute()
+                                              maxResults=10, singleEvents=True,
+                                              orderBy='startTime').execute()
         events = events_result.get('items', [])
-        
+
         if not events:
             print('No upcoming events found.')
-            lbl_showEvents["text"] = lbl_showEvents["text"] + '\nNo upcoming events found.'
+            lbl_showEvents["text"] = lbl_showEvents[
+                                         "text"] + '\nNo upcoming events found.'
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
             print(start, event['summary'])
-            lbl_showEvents["text"] = lbl_showEvents["text"] + '\n' + start + ' ' + event['summary']
-        
-    #Split to UI
-    
-    window.columnconfigure([0,], minsize=50, weight=1)
+            lbl_showEvents["text"] = lbl_showEvents[
+                                         "text"] + '\n' + start + ' ' + event[
+                                         'summary']
+
+    # Split to UI
+    window.columnconfigure([0, ], minsize=50, weight=1)
     window.rowconfigure([0, 1], minsize=50, weight=1)
-   
+
     lbl_showEvents = tk.Label(master=window, text="No upcoming events found.")
     lbl_showEvents.grid(row=0, column=0)
-    
-    btn_refresh = tk.Button(master=window, text="Refresh Events", command=getEvents)
+    btn_refresh = tk.Button(master=window, text="Refresh Events",
+                            command=get_events)
     btn_refresh.grid(row=1, column=0, sticky="nsew")
-    
-    getEvents()
-    
+
+    get_events()
     window.mainloop()
 
 
